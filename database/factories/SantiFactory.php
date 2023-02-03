cb4011f3bbecd6e03091310dd7d474c84cb5561b
danipetro21 <idany346@gmail.com>
2023-02-03 10:23:34 +0100

    add factory and seeders

---
+++ database/factories/SantiFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Faker\Guesser\Name;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class SantiFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake() -> firstName(),
+            'birdPlace' => fake() -> lastName(),
+            'blessingDate' => fake() -> date(),
+            'miracleCount' => fake() -> numberBetween(1, 99),
+        ];
+    }
+}
---
+++ database/seeders/SantiSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Santi;
+
+class SantiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Santi::factory() -> count(200) -> create();
+    }
+}
